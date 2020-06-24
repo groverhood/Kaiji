133f01f82d2f7e0f978be027ad087f29c18cc45e
Duncan Huntsinger <aloginna@cs.utexas.edu>
2020-06-23 23:02:22 -0500

    Now loads the kernel (seeming not upper half?)

--- src/upper_half.c
+++ src/upper_half.c
@@ -5,6 +5,7 @@
 #define funcall(fun, arg) asm volatile ("mov x0, %1\n\t"    \
                                         "blr %0" :: "r" (fun), "r" (arg))
 #elif defined(__amd64__)
+#include <X64/ProcessorBind.h>
 #define funcall(fun, arg) asm volatile ("call %0" :: "r" (fun), "D" (arg))
 #endif
 
@@ -41,15 +42,15 @@ void boot_upper_half(EFI_BOOT_SERVICES *services, EFI_RUNTIME_SERVICES *vmapper,
     vmapper->SetVirtualAddressMap(bootinfo->memmap_size, sizeof *bootinfo->memmap, EFI_MEMORY_DESCRIPTOR_VERSION,
                                   bootinfo->memmap);
 
-    EFI_PHYSICAL_ADDRESS pkernhdr = (EFI_PHYSICAL_ADDRESS)kernhdr + UPPER_HALF_BASE;
-    kernhdr = (Elf64_Ehdr *)pkernhdr;
+    vmapper->ConvertPointer(0, (void **)&kernhdr);
+    vmapper->ConvertPointer(0, (void **)&bootinfo);
+    vmapper->ConvertPointer(0, (void **)&bootinfo->ramdisk);
 
-    EFI_PHYSICAL_ADDRESS pbootinfo = (EFI_PHYSICAL_ADDRESS)bootinfo + UPPER_HALF_BASE;
-    bootinfo = (struct bootstruct *)pbootinfo;
+    cout->OutputString(cout, u"Kernel entry point at ");
+    printn((UINTN)((EFI_VIRTUAL_ADDRESS)kernhdr + kernhdr->e_entry));
+    cout->OutputString(cout, u"\n\r");
 
-    bootinfo->ramdisk = bootinfo->ramdisk + UPPER_HALF_BASE;
-
-    start_fn local_start = (start_fn)(pkernhdr + kernhdr->e_entry);
+    start_fn local_start = (start_fn)((EFI_VIRTUAL_ADDRESS)kernhdr + kernhdr->e_entry);
     funcall(local_start, bootinfo);
 }
 
@@ -60,8 +61,7 @@ static void init_upper_half_mapping(struct bootstruct *bootinfo)
     for (i = 0; i < bootinfo->memmap_size; i += bootinfo->memmap_entsz) {
         EFI_MEMORY_DESCRIPTOR *memdesc = (EFI_MEMORY_DESCRIPTOR *)(pmemmap + i);
         memdesc->VirtualStart = memdesc->PhysicalStart + UPPER_HALF_BASE;
-        printn(pmemmap + i);
-        cout->OutputString(cout, u"\n\rMapping 0x");
+        cout->OutputString(cout, u"Mapping 0x");
         printn(memdesc->PhysicalStart);
         cout->OutputString(cout, u" -> 0x");
         printn(memdesc->VirtualStart);
